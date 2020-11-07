ba0e0fb82f5e143158ea793ad5fb6859aaec22fa
Alex <tictak211@gmail.com>
2020-11-07 22:57:46 +0200

    koa-router

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+
+// === env ===
+import dotenv from 'dotenv';
+
+// === graphql ===
 import mount from 'koa-mount';
 import graphqlHTTP from 'koa-graphql';
 import schema from './graphql/schema.js';
+
+// === db ===
 import initDB from './database/index.js';
-import dotenv from 'dotenv';
 
 dotenv.config();
-
-const app = new Koa();
-
 initDB();
 
-app.use(
-  mount(
-    '/graphql',
-    graphqlHTTP({
-      schema: schema,
-      graphiql: true,
-    }),
-  ),
-);
+const app = new Koa();
+const router = new Router({ prefix: '/api' });
+
+router
+  .get('/', ctx => {
+    ctx.body = 'Swagger here';
+  })
+  .get('/users', ctx => {
+    ctx.body = 'Users here';
+  });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(
+    mount(
+      '/graphql',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: true,
+      }),
+    ),
+  );
 
 app.listen(process.env.PORT || 5000);
 
