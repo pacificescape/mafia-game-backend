4868872a0ade5ec923146edc759b80da8e9c3dbc
Alex <tictak211@gmail.com>
2020-11-11 12:19:21 +0200

    basic queries
    getById
    getByName
    getAll

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,13 @@
-import { GraphQLString, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import {
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLInt,
+} from 'graphql';
 import userGraphQLType from './user.type';
 import User from '../models/user.model';
+import UserType from './user.type';
 
 const UserQuery = new GraphQLObjectType({
   name: 'UserQuery',
@@ -10,6 +17,20 @@ const UserQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(_, args) {
         return User.findById(args.id);
+      },
+    },
+    getUserByName: {
+      type: userGraphQLType,
+      args: { name: { type: GraphQLString } },
+      resolve(_, args) {
+        return User.findOne({ name: args.name });
+      },
+    },
+    getUsers: {
+      type: new GraphQLList(UserType),
+      args: { limit: { type: GraphQLInt } },
+      resolve(_, { limit }) {
+        return User.find({}).limit(limit);
       },
     },
   },
