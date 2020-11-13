f019600b68353052a82b125a6d674dc7f8ea8ae4
Alex <tictak211@gmail.com>
2020-11-13 23:23:27 +0200

    new queries and new folders structure

---
+++ src/graphql/queries/user.query.ts
@@ -0,0 +1,58 @@
+import {
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInt,
+} from 'graphql';
+import User from '../../models/user.model';
+import UserType from '../types/user.type';
+
+const UserQuery: GraphQLObjectType = new GraphQLObjectType({
+  description: 'Query to interact with `Users` collection',
+  name: 'UserQuery',
+  fields: {
+    getUserById: {
+      description:
+        'Returns a `User` where the argument `id` is equal to `User.id` in database.',
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(_, { id }) {
+        return User.findById(id);
+      },
+    },
+    getUserByName: {
+      description:
+        'Returns a `User` where the argument `name` is equal to `User.name` in database.',
+      type: UserType,
+      args: { name: { type: GraphQLString } },
+      resolve(_, { name }) {
+        return User.findOne({ name });
+      },
+    },
+    getUsers: {
+      description:
+        'Returns a list of users. Use the `limit` argument to get only first `N` users.',
+      type: new GraphQLList(UserType),
+      args: { limit: { type: GraphQLInt } },
+      resolve(_, { limit }) {
+        return User.find({}).limit(limit);
+      },
+    },
+    countUsers: {
+      description: 'Return the count of `User` documents.',
+      type: GraphQLInt,
+      resolve() {
+        return User.find({}).estimatedDocumentCount();
+      },
+    },
+    addUser: {
+      description: 'Creates and adds new user.',
+      type: GraphQLInt,
+      resolve() {
+        return User.find({}).estimatedDocumentCount();
+      },
+    },
+  },
+});
+
+export default UserQuery;
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,48 +1,10 @@
-import {
-  GraphQLString,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLList,
-  GraphQLInt,
-} from 'graphql';
-import userGraphQLType from './user.type';
-import User from '../models/user.model';
-import UserType from './user.type';
+import { GraphQLSchema } from 'graphql';
 
-const UserQuery = new GraphQLObjectType({
-  description: 'A type that describes the User.',
-  name: 'UserQuery',
-  fields: {
-    getUserById: {
-      description:
-        'Returns a User where the argument `id` is equal to `User.id` in database.',
-      type: userGraphQLType,
-      args: { id: { type: GraphQLString } },
-      resolve(_, args) {
-        return User.findById(args.id);
-      },
-    },
-    getUserByName: {
-      description:
-        'Returns a User where the argument `name` is equal to `User.name` in database.',
-      type: userGraphQLType,
-      args: { name: { type: GraphQLString } },
-      resolve(_, args) {
-        return User.findOne({ name: args.name });
-      },
-    },
-    getUsers: {
-      description:
-        'Returns a list of users. Use the `limit` argument to get only first `N` users.',
-      type: new GraphQLList(UserType),
-      args: { limit: { type: GraphQLInt } },
-      resolve(_, { limit }) {
-        return User.find({}).limit(limit);
-      },
-    },
-  },
-});
+// === queries ===
+import UserQuery from './queries/user.query';
 
-export default new GraphQLSchema({
+const schema = new GraphQLSchema({
   query: UserQuery,
 });
+
+export default schema;
