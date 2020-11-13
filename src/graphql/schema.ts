45e2d3a5787a09481ba4343b339084bff469cbde
Alex <tictak211@gmail.com>
2020-11-13 23:48:02 +0200

    added missing types

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -3,7 +3,7 @@ import { GraphQLSchema } from 'graphql';
 // === queries ===
 import UserQuery from './queries/user.query';
 
-const schema = new GraphQLSchema({
+const schema: GraphQLSchema = new GraphQLSchema({
   query: UserQuery,
 });
 
