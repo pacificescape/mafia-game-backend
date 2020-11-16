33c6efed9f5a95d391492daa6858c88bf10b535d
Alex <tictak211@gmail.com>
2020-11-16 22:21:17 +0200

    get/set for character entity

--- src/entities/character.entity.ts
+++ src/entities/character.entity.ts
@@ -1,9 +1,33 @@
 import { ICharacterBase } from 'src/shared/interfaces/characters/character-base.interface';
 
-export class Character implements ICharacterBase {
-  public isAlive: boolean = false;
-  public hasShield: boolean = false;
-  public isSick: boolean = false;
+export abstract class Character implements ICharacterBase {
+  private _isAlive: boolean = false;
+  private _hasShield: boolean = false;
+  private _isSick: boolean = false;
 
   constructor(public readonly id: string) {}
+
+  public get isAlive(): boolean {
+    return this._isAlive;
+  }
+
+  public set isAlive(value: boolean) {
+    this._isAlive = value;
+  }
+
+  public get hasShield(): boolean {
+    return this._hasShield;
+  }
+
+  public set hasShield(value: boolean) {
+    this._hasShield = value;
+  }
+
+  public get isSick(): boolean {
+    return this._isSick;
+  }
+
+  public set isSick(value: boolean) {
+    this._isSick = value;
+  }
 }
