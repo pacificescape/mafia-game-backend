f429848f266ff9625460ac0af9cd6b28c497c8e1
Alex <tictak211@gmail.com>
2020-11-22 14:00:13 +0200

    interface improvement

--- src/shared/interfaces/customContext.interface.ts
+++ src/shared/interfaces/customContext.interface.ts
@@ -1,11 +1,14 @@
+import { BaseContext } from 'koa';
 import { IDb } from './db.interface';
 
-export interface ICustomAppContext {
+export interface ICustomAppContext extends BaseContext {
   db: IDb;
+  websocket: any;
 }
 
 export interface ICustomAppState {
-  db: string;
+  db: IDb;
+  websocket: any;
 }
 
 // https://stackoverflow.com/questions/43160598/adding-properties-to-koa2s-context-in-typescript
